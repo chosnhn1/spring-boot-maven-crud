f6ffee6be20c076cc893b6a677d542b913f0677a
Cho Seonghan <chosnhn1@gmail.com>
2023-03-09 20:24:07 +0900

    initial commit

---
+++ src/main/java/com/example/springbootmavencrud/SpringBootMavenCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmavencrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootmavencrud/SpringBootMavenCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmavencrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMavenCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
